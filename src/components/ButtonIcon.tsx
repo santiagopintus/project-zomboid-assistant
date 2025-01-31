0a4b4c029601f80bac301deb865d59cd85f91f8a
Santiago Pintus <santiagopintus@gmail.com>
2025-01-30 21:18:03 -0300

    Fixed linting errors

--- src/components/ButtonIcon.tsx
+++ src/components/ButtonIcon.tsx
@@ -1,8 +1,20 @@
 import React from "react";
 
-const ButtonIcon = ({ children, onClick, ...props }: any) => {
+const ButtonIcon = ({
+  children,
+  ...props
+}: {
+  children: React.ReactNode;
+  [key: string]:
+    | React.DetailedHTMLProps<
+        React.ButtonHTMLAttributes<HTMLButtonElement>,
+        HTMLButtonElement
+      >
+    | React.ReactNode
+    | (() => void);
+}) => {
   return (
-    <button className="button-icon" onClick={onClick} {...props}>
+    <button className="button-icon" {...props}>
       {children}
     </button>
   );
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,19 +1,28 @@
+import { ToDoItem } from "@/app/to-do/page";
+
 // Helper function to transform data from JSON to string and string to JSON
-const transformData = (data: any, toJson: boolean): any => {
-  return toJson ? JSON.stringify(data) : JSON.parse(data);
+type SaveableData = ToDoItem;
+
+const transformData = (
+  data: string | SaveableData[],
+  toJson: boolean
+): string | SaveableData[] => {
+  return toJson
+    ? JSON.stringify(data as SaveableData[])
+    : JSON.parse(data as string);
 };
 
 // Function to save data into localStorage
-export const saveToLocalStorage = (key: string, data: any): void => {
-  const stringData = transformData(data, true);
+export const saveToLocalStorage = (key: string, data: SaveableData[]): void => {
+  const stringData = transformData(data, true) as string;
   localStorage.setItem(key, stringData);
 };
 
 // Function to retrieve data from localStorage
-export const getFromLocalStorage = (key: string): any => {
+export const getFromLocalStorage = (key: string): SaveableData[] | null => {
   const stringData = localStorage.getItem(key);
   if (stringData) {
-    return transformData(stringData, false);
+    return transformData(stringData, false) as SaveableData[];
   }
   return null;
 };
